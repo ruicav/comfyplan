e6a66a62cf12b660399c771d9a95b2464a82f2e0
rui.alves <rui.alves@zallpy.com>
2020-01-16 01:10:45 -0300

    Lint!

--- index.js
+++ index.js
@@ -13,49 +13,49 @@ const server = http.createServer(app);
 const port = 9000;
 
 const isValid = ({ row = {}, schema = {} }) => {
-  const errors = []
+  const errors = [];
   for (const columnKey in row) {
     if (schema[columnKey]) {
-        for(const validation of schema[columnKey]) {
-          switch(validation.type) {
-            case 'min':
-              if(Number(row[columnKey]) < Number(validation.value)) {
-                errors.push('Valor minimo invalido')
-              }
-            case 'max':
-              if(Number(row[columnKey]) > Number(validation.value)) {
-                errors.push('Valor maximo invalido')
-              }
-          }
+      for (const validation of schema[columnKey]) {
+        switch (validation.type) {
+          case 'min':
+            if (Number(row[columnKey]) < Number(validation.value)) {
+              errors.push('Valor minimo invalido');
+            }
+          case 'max':
+            if (Number(row[columnKey]) > Number(validation.value)) {
+              errors.push('Valor maximo invalido');
+            }
         }
-        console.log('errors', errors)
-        return errors.length === 0;
+      }
+      console.log('errors', errors);
+      return errors.length === 0;
     }
   }
-  console.log('row without validation in schema')
+  console.log('row without validation in schema');
   return true;
-}
+};
 
-
-router.post('/', upload.single('file'), function (req, res) {
+router.post('/', upload.single('file'), function(req, res) {
   //req.file.path
   // const headers = schema.map(column => column && Object.keys(column)[0])
-  csv.parseFile(req.file.path, { headers: true, maxRows: 3 })
-    .validate( row => {
-      console.log('row', `>${JSON.stringify(row)}<`) 
+  csv
+    .parseFile(req.file.path, { headers: true, maxRows: 3 })
+    .validate(row => {
+      console.log('row', `>${JSON.stringify(row)}<`);
     })
-    .on("data-invalid", (wrongOne, index) => console.log(`wrongOne ${wrongOne}`))
+    .on('data-invalid', (wrongOne, index) => console.log(`wrongOne ${wrongOne}`))
     .on('error', error => console.error(error))
     .on('data', row => console.log(row))
     .on('end', rowCount => console.log(`Parsed ${rowCount} rows`));
-  
+
   return;
-}); 
+});
 
 app.use('/upload-csv', router);
 
 function startServer() {
-  server.listen(port, function () {
+  server.listen(port, function() {
     console.log('Express server listening on ', port);
   });
 }
